cac02cfac7ae3db0f040e47d6638aec7fb2ae271
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-07-03 12:30:04 +0300

    feat: добавлена модель задания

--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+  title = models.CharField(max_length=255, verbose_name="Название")
+  description = models.TextField(verbose_name="Описание")
+  expected_output = models.TextField(verbose_name="Ожидаемый вывод")
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    verbose_name = "Задача"
+    verbose_name_plural = "Задачи"
