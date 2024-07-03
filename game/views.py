5ef9e8429069186f7ecf3fbc6feccb992d3d5f84
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-07-03 12:35:00 +0300

    feat: добавлена разводящая

--- game/views.py
+++ game/views.py
@@ -1,13 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, Http404, render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .models import Task
+
+
+class CodegolfListView(ListView):
+  """Страница с codegolf"""
+
+  model = Task
+  template_name = 'list.html'
+
+  def get_context_data(self, *args, **kwargs):
+    context = super().get_context_data(*args, **kwargs)
+
+    return context
+
 
 class CodegolfPageView(TemplateView):
   """Страница с codegolf"""
 
-  template_name = 'index.html'
+  template_name = 'id.html'
 
   def get_context_data(self, *args, **kwargs):
     context = super().get_context_data(*args, **kwargs)
