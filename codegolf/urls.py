fe2ebc52926bd2422f2e73a73d241ac5fb4cd1c4
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-07-03 12:06:09 +0300

    refactor: вьюха перенесена в другое приложение

--- codegolf/urls.py
+++ codegolf/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.CodegolfPageView.as_view()),
     path("game/", include("game.urls")),
     path("admin/", admin.site.urls),
 ]
