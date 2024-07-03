dd714777ec6b90fdd7c8838c857d11989ac14adf
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-07-03 12:30:04 +0300

    feat: добавлена модель задания

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CodegolfPageView.as_view(), name="game_index"),
+    path("<int:id>/", views.CodegolfPageView.as_view(), name="game_task"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, Http404, render
 from django.views.generic.base import TemplateView
+
+from .models import Task
 
 class CodegolfPageView(TemplateView):
   """Страница с codegolf"""
@@ -9,10 +12,15 @@ class CodegolfPageView(TemplateView):
   def get_context_data(self, *args, **kwargs):
     context = super().get_context_data(*args, **kwargs)
 
+    task = get_object_or_404(
+      Task,
+      id=kwargs.get('id'),
+    )
+
     context.update({
-      'task_id': 1,
-      'task_title': '99 Bottles of Beer',
-      'task_description': f"Print the lyrics to the song 99 Bottles of Beer:<br>99 bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, 98 bottles of beer on the wall.<br>98 bottles of beer on the wall, 98 bottles of beer.Take one down and pass it around, 97 bottles of beer on the wall.<br>…<br>1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.<br>No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.",
+      'task_id': task.id,
+      'task_title': task.title,
+      'task_description': task.description,
     })
 
     return context
