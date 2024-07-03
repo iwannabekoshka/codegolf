6df0bfe94b5b2dbe4cd5f1eefed993a99002bd38
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-07-03 12:35:00 +0300

    feat: добавлена разводящая

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.CodegolfListView.as_view(), name="game_list"),
     path("<int:id>/", views.CodegolfPageView.as_view(), name="game_task"),
 ]
