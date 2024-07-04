85f743b39c597ae02b47a378ad23c687988d6280
St-Old-Recursion <tima.makaev@gmail.com>
2024-07-04 17:51:12 +0300

    переработка models

--- game/models.py
+++ game/models.py
@@ -1,30 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from .constants import *
+
 
 class Task(models.Model):
-  title = models.CharField(max_length=255, verbose_name="Название")
-  description = models.TextField(verbose_name="Описание")
-  expected_output = models.TextField(verbose_name="Ожидаемый вывод")
+    title = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    expected_output = models.TextField(verbose_name="Ожидаемый вывод", default='')
+    example_code = models.TextField('Код примера', default='')
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-  class Meta:
-    verbose_name = "Задача"
-    verbose_name_plural = "Задачи"
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
 
 
-CODE_LANGUAGES = (
-  ('python', 'Python'),
-  ('javascript', 'JS'),
-)
 class Answer(models.Model):
-  task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='answers', verbose_name="Задача")
-  code = models.TextField(verbose_name="Ответ")
-  code_lang = models.CharField(max_length=255, verbose_name="Язык программирования", default=None, null=True, choices=CODE_LANGUAGES)
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='answers', verbose_name="Задача"
+    )
+    code = models.TextField(verbose_name="Ответ")
+    code_lang = models.CharField(
+        max_length=255, verbose_name="Язык программирования", default='', null=True, choices=CODE_LANGUAGES
+    )
+    code_result = models.TextField('Результат', default='')
+    code_len = models.PositiveIntegerField('Длина ответа', default=0)
+    username = models.CharField('username', max_length=255, default='')
+    is_correct = models.BooleanField('Ответ верен', default=False)
 
-  def __str__(self):
-    return f"{self.pk}"
+    def __str__(self):
+        return f"{self.pk}"
 
-  class Meta:
-    verbose_name = "Ответ"
-    verbose_name_plural = "Ответы"
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
--- game/views.py
+++ game/views.py
@@ -10,51 +10,63 @@ from .utils import execute_cpp_code
 
 
 class CodegolfListView(ListView):
-  """Страница со списком заданий"""
+    """Страница со списком заданий"""
 
-  model = Task
-  template_name = 'list.html'
+    model = Task
+    template_name = 'list.html'
 
 
 class CodegolfPageView(DetailView):
-  """Страница задания"""
+    """Страница задания"""
 
-  model = Task
-  template_name = 'id.html'
-  context_object_name = 'task'
+    model = Task
+    template_name = 'id.html'
+    context_object_name = 'task'
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    context.update({
-      'code_languages': CODE_LANGUAGES
-    })
+        context.update({
+            'code_languages': CODE_LANGUAGES
+        })
 
-    return context
+        return context
 
-  def post(self, request, *args, **kwargs):
-    data = json.loads(request.body.decode('utf-8'))
-    code = data.get('code')
-    code_lang = data.get('code_lang')
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body.decode('utf-8'))
+        try:
+            code = data['code']
+            code_lang = data['code_lang']
+            username = data['username']
+            code_len = data['code_len']
+        except KeyError:
+            return JsonResponse({'status': 'error', 'message': 'Отсутствуют необходимые данные'}, status=400)
 
-    # if not code:
-    #   return JsonResponse({'status': 'error', 'message': 'Отсутствуют необходимые данные'}, status=400)
+        task_obj = self.get_object()
 
-    exec_data = execute_cpp_code(code)
-    if 'error' in exec_data:
-      response = {
-        'status': 'error',
-        'userOutput': f"{exec_data['error']}: {exec_data['details']}"
-      }
-    else:
-      response = {
-        'status': 'success',
-        'userOutput': exec_data['output']
-      }
-    response.update({
-      'expectedOutput': self.get_object().expected_output,
-    })
-    #answer = Answer(task=self.get_object(), code=code, code_lang=code_lang)
-    #answer.save()
+        exec_data = execute_cpp_code(code)
+        if 'error' in exec_data:
+            response = {
+                'status': 'error',
+                'userOutput': f"{exec_data['error']}: {exec_data['details']}"
+            }
+        else:
+            output = exec_data['output'].replace('\r\n', '\n').strip()
+            response = {
+                'status': 'success',
+                'userOutput': output
+            }
+            is_correct = output == task_obj.expected_output
+            answer = Answer.objects.create(
+                task=task_obj,
+                code=code,
+                code_lang=code_lang,
+                code_result=output,
+                username=username,
+                is_correct=is_correct,
+            )
+        response.update({
+            'expectedOutput': task_obj.expected_output,
+        })
 
-    return JsonResponse(response)
+        return JsonResponse(response)
